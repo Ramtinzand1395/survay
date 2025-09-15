7a6592c4aea662888a3761799a9c948458d508bf
ramtin.zand <ramtinzand6@gmail.com>
2025-09-15 12:42:18 +0330

    Url Added3

---
+++ frontend/app/error.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className="container">
+      <h2>مشکلی پیش آمده.</h2>
+      <button onClick={() => reset()}>دوباره تلاش کن.</button>
+    </div>
+  );
+}
